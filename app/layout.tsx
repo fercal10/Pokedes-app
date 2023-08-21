97a105407ee1abb71eb7b271d64722ad62986abd
fercal10 <101475122+fercal10@users.noreply.github.com>
2023-08-21 03:23:36 -0400

    update 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import clsx from "clsx";
 
 export const metadata: Metadata = {
 	title: {
-		default: siteConfig.name,
-		template: `%s - ${siteConfig.name}`,
+		default: "Pokedex",
+		template: "",
 	},
-	description: siteConfig.description,
+	description: "looking for pokemons",
 	themeColor: [
 		{ media: "(prefers-color-scheme: light)", color: "white" },
 		{ media: "(prefers-color-scheme: dark)", color: "black" },
